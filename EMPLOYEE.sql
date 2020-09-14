6389f481a7bf669087f9af21002e2d8b5877ddf2
sushantsc2014 <sushantsc2014@gmail.com>
2020-09-15 01:32:34 +0530

    created tables in db

--- EMPLOYEE.sql
+++ EMPLOYEE.sql
@@ -7,3 +7,5 @@ create database EMPLOYEE owner employer;
 grant connect on database employee to employer;
 
 grant all privileges on database employee to employer;
+
+psql -U employer -d employee< C:\git\PostgreSQL\Employee_db_sql_files\create_tables.sql
