aadb21583aac24c1334e48715072609dff4f1cf4
sushantsc2014 <sushantsc2014@gmail.com>
2020-10-31 14:17:54 +0530

    forcing primary key constraint through UNIQUE INDEX

--- indexes_explained.sql
+++ indexes_explained.sql
@@ -17,3 +17,39 @@ explain select * from salaries where salary=81025
 
 "Index Scan using emp_salary_idx on salaries  (cost=0.28..8.29 rows=1 width=16)"
 "  Index Cond: (salary = 81025)"
+
+---------------------
+
+create table trial (seq int, value int)
+
+insert into trial values (1,23),(2, 444), (2, 4448)
+
+create unique index on trial (seq) --error
+/*
+ERROR:  could not create unique index "trial_seq_idx"
+DETAIL:  Key (seq)=(2) is duplicated.
+SQL state: 23505
+*/
+
+delete from trial where value=4448
+
+insert into trial values (3, 4448),(4,978)
+
+select * from trials
+1	23
+2	444
+3	4448
+4	978
+
+create unique index on trial (seq)
+
+select * from pg_indexes where tablename='trial'
+"public"	"trial"	"trial_seq_idx"		"CREATE UNIQUE INDEX trial_seq_idx ON public.trial USING btree (seq)"
+
+
+insert into trial values (3,999)
+/*
+ERROR:  duplicate key value violates unique constraint "trial_seq_idx"
+DETAIL:  Key (seq)=(3) already exists.
+SQL state: 23505
+*/
