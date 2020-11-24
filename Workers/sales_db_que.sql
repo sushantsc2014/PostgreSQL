80fc20be31169d1e2544898807c83d92fecedc1f
sushantsc2014 <sushantsc2014@gmail.com>
2020-11-24 20:29:27 +0530

    ANY use

--- Workers/sales_db_que.sql
+++ Workers/sales_db_que.sql
@@ -229,3 +229,46 @@ select order_date, sum(purchase_amnt) over(partition by order_date), (max(purcha
 "2012-10-05"	215.76				1150.5
 "2012-10-10"	4463.83				3480.4  ---->
 "2012-10-10"	4463.83				3480.4
+
+/* 16. Write a query to find the salesmen who have multiple customers */
+select distinct(c1.salesman_id) from customer c1 where c1.salesman_id=(select salesman_id from customer where 
+c1.salesman_id=salesman_id and c1.customer_id<>customer_id)
+5001
+5002
+
+
+/* 17. Write a query to find all the salesmen who worked for only one customer. */	
+
+select 	salesman_id from salesman where salesman_id not in
+(select distinct(c1.salesman_id) from customer c1 where c1.salesman_id=(select salesman_id from customer where 
+c1.salesman_id=salesman_id and c1.customer_id<>customer_id))
+5005
+5006
+5007
+5003
+
+/* 18. Write a query that extract the rows of all salesmen who have customers with more than one orders. */	
+
+select distinct(o1.salesman_id) from orders o1 where o1.salesman_id in (select salesman_id from orders where o1.customer_id=customer_id and o1.order_no<>order_no)
+5001
+5002
+5003
+
+/* 23. Write a query to display all the orders that had amounts that were greater than at least one of the orders on September 10th 2012.  */
+
+select * from orders o1 where o1.purchase_amnt >any (select o2.purchase_amnt from orders o2 where order_date='10-09-2012')
+70007	948.5	"2012-09-10"	3005	5002
+70005	2400.6	"2012-07-27"	3007	5001
+70008	5760	"2012-09-10"	3002	5001
+70010	1983.43	"2012-10-10"	3004	5006
+70003	2480.4	"2012-10-10"	3009	5003
+70013	3045.6	"2012-04-25"	3002	5001
+
+/* 24. Write a query to find all orders with an amount smaller than any amount for a customer in London. */
+
+select * from orders where purchase_amnt < any (select o2.purchase_amnt from orders o2 where o2.customer_id in (select customer_id from customer where city='London'))
+70001	150.5	"2012-10-05"	3005	5002
+70002	65.26	"2012-10-05"	3002	5001
+70004	110.5	"2012-08-17"	3009	5003
+70012	250.45	"2012-06-27"	3008	5002
+70011	75.29	"2012-08-17"	3003	5007											
